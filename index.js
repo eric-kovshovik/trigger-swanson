523ca047a958f6b15789c5ed67ae827f034b69df
Eric Kovshovik <jaroslav.kovshovik@gmail.com>
2019-02-02 14:11:37 -0700

    Final app triggered by url

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ exports.handler = function(event, context) {
       url: 'http://ron-swanson-quotes.herokuapp.com/v2/quotes',
       method: 'GET',
     }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var obj = JSON.parse(body);
-        var quote = obj[0]
-        var sms_params = {
-            Message: quote,
-            Subject: "Test SNS From Lambda",
-            TopicArn: process.env.TOPIC_ARN
-        };
-        sns.publish(sms_params, context.done);
-        };
+        if (!error && response.statusCode == 200) {
+       
+            var obj = JSON.parse(body);
+            var quote = obj[0]
+            var sms_params = {
+                Message: quote,
+                Subject: "Test SNS From Lambda",
+                TopicArn: process.env.TOPIC_ARN
+            };
+            sns.publish(sms_params, context.done);
+        }
       }
-    )
+    );    
 };
 
